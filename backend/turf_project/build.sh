3e4f06337a641175faaeafc462662f91ef32a436
Manish Dange <dangemanish35@gmail.com>
2026-04-06 02:41:20 +0530

    feat: auto create superuser on deploy

--- backend/turf_project/build.sh
+++ backend/turf_project/build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+python backend/turf_project/manage.py collectstatic --no-input
+python backend/turf_project/manage.py migrate
+python backend/turf_project/manage.py createsu  
