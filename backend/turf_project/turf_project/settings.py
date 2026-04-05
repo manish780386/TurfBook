8356c6361561b818be8bf9d0369b818bea60b3f8
Manish Dange <dangemanish35@gmail.com>
2026-04-06 01:39:35 +0530

    fix: move runtime.txt and requirements.txt to root

--- backend/turf_project/turf_project/settings.py
+++ backend/turf_project/turf_project/settings.py
@@ -81,6 +81,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://127.0.0.1:3000',
     'http://localhost:5174',
+   'https://turfbook-umber.vercel.app/',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
